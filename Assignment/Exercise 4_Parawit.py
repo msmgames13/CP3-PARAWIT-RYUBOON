380d43a2b15899dac968129f14fd701980d9c8c2
msmgames13 <msmgames2@gmail.com>
2026-02-02 17:57:25 +0700

    Create Exercise 4 _Parawit

---
+++ Assignment/Exercise 4_Parawit.py
@@ -0,0 +1,16 @@
+a=90
+b=89
+c=88
+d=87
+e="Foundationenglish"
+f="Feneralbusiness"
+g="Introductiontocomputersystems"
+h="Computerprogramming"
+
+print(e,"Total",a,)
+print('''--------------''')
+print(f,"Total",b,)
+print('''--------------''')
+print(g,"Total",c,)
+print('''--------------''')
+print(h,"Total",d,)
